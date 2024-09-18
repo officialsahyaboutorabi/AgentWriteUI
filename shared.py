3c4f5776b361e51e9ff405f2d4d1ac763b757606
officialsahyaboutorabi <152934862+officialsahyaboutorabi@users.noreply.github.com>
2024-09-18 10:51:05 +1000

    Update shared.py
    Updated this module to work with imagetostory.py

--- shared.py
+++ shared.py
@@ -10,3 +10,12 @@ def get_selected_provider():
 
 def get_selected_model_name():
     return st.session_state.get('last_inputs', {}).get('llm_model', '')
+
+def get_numberofwords():
+    return st.session_state.get('num_words', {}).get('num_words', '')
+
+def get_uploaded_file():
+    return st.session_state.get('uploaded_file')
+
+def get_image():
+    return st.session_state.get('uploaded_image')
